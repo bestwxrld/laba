d0b0d39fcac4aecd93fd134df6f2cced17a41ee2
gotika <sonya.gotika@mail.ru>
2024-11-19 23:40:46 +0300

    initial

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, View, StyleSheet, RefreshControl } from 'react-native';
+import ProgrammingCard from './ProgrammingCard';
+import programmingLanguages from './data';
 
 export default function App() {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000); // Имитация обновления данных
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={programmingLanguages}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ProgrammingCard
+            language={item.language}
+            experience={item.experience}
+            logo={item.logo}
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </View>
   );
 }
@@ -14,7 +37,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ data.js
@@ -0,0 +1,22 @@
+const programmingLanguages = [
+    {
+      id: 1,
+      language: 'JavaScript',
+      experience: '2 года',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    },
+    {
+      id: 2,
+      language: 'Python',
+      experience: 'нет',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+    },
+    {
+      id: 3,
+      language: 'Go',
+      experience: '6 месяцев',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Go_Logo_Blue.svg',
+    },
+  ];
+  
+  export default programmingLanguages;
